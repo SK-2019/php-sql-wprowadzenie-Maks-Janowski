c8ec7118b1af6825777f89b26d989536813fbc85
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-10-30 13:43:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,80 @@
+<h1>Maksymilian Janowski 2Ti</h1>
 <?php
-    
-    echo("<div style='font-size:30px; margin-bottom:20px'><b>Maksymilian Janowski 2Ti nr11</b></div>");
-    echo("<hr />");
-    function tabelka($zapytanie, $nazwa, $kolumna, $row1){
-    include("connect.php");
-    $result=$conn->query($zapytanie);
-    echo("<div style='font-size:20px; margin-bottom:5px'>$nazwa</div>");
-    echo("<table border=1 style='font-size:20px; margin-bottom:20px'>");
-        echo("<th>$kolumna</th>");
-        echo("<th>Dział</th>");
+   require_once('conn.php');
+  
+    $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org');
+    $result=$conn->query($sql);
+        echo("<hr />");
+        echo("<h3>Tabela Pracowników</h3>");
+        echo("<li>SQL: $sql");
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa_dzial</th>");
+        echo("<th>data_urodzenia</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row[$row1]."</td><td>".$row['dzial']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
                 echo("</tr>");
             }
-    echo("</table>");
-    }
+        echo("</table>");
+    echo("<hr />");
+    $sql=('SELECT * from pracownicy,organizacja where dzial=id_org and imie like "%a"');
+    $result=$conn->query($sql);
+        echo("<h3>Tabela Kobiet</h3>");//nazwa nad tabelą
+        echo("<li>SQL: $sql");
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa_dzial</th>");
+        echo("<th>data_urodzenia</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+echo("<hr />");
+    $sql=('SELECT * from pracownicy,organizacja where dzial=id_org  order by imie asc');
+    $result=$conn->query($sql); //mysql
+        echo("<h3>Tabela Pracowników Posortowana Alfabetycznie</h3>");//nazwa nad tabelą
+        echo("<li>SQL: $sql");
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa_dzial</th>");
+        echo("<th>data_urodzenia</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+echo("<hr />");
+    $sql=('SELECT * from pracownicy,organizacja where dzial=id_org  order by zarobki asc');
+    $result=$conn->query($sql); //mysql
+        echo("<h3>Tabela Pracowników Posortowana Zarobkami Rosnąco</h3>");//nazwa nad tabelą
+        echo("<table border=1>");
+        echo("<li>SQL: $sql");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa_dzial</th>");
+        echo("<th>data_urodzenia</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+echo("<hr />");
 
-    tabelka("select avg(zarobki) as myavg, dzial from pracownicy group by dzial", "Średnie zarobki pracowników w poszczególnych działach:", "Średnia", "myavg");
-    echo("<hr />");
-    tabelka("select count(zarobki) as mycount, dzial from pracownicy where imie like '%a' group by dzial", "Liczba mężczyzn w poszczególnych działach:", "Liczba", "mycount");
-    echo("<hr />");
-    tabelka("select max(zarobki) as mymax, dzial from pracownicy where dzial=2 group by dzial", "Maksymalne zarobki z działu 2:", "Maks", "mymax");
-    echo("<hr />");
-    tabelka("select min(zarobki) as mymin, dzial from pracownicy where imie not like '%a' group by dzial", "Minimalne zarobki mężczyzn:", "Min", "mymin");
-    echo("<hr />");
-    tabelka("select sum(zarobki) as mysum, dzial from pracownicy where dzial=1 or dzial=2 group by dzial", "Suma zarobków z działu 1 i 2:", "Suma", "mysum");
-    echo("<hr />");
-    tabelka("select avg(zarobki) as myavg, dzial from pracownicy group by dzial having myavg<35", "Średnie zarobki pracowników w poszczególnych działach mniejsze od 35:", "Średnia", "myavg");
-    echo("<hr />");
 
 ?>
