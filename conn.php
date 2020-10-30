a47f987baabeee7343e0c36e60261e02097defe6
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-10-30 13:40:09 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -5,10 +5,8 @@ $username = "WtbCi7xfxi";
 $password = "BDDTxIAkhC";
 $dbname = "WtbCi7xfxi";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+$conn=new mysqli($servername, $username, $password, $dbname);
+        if($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 ?>
