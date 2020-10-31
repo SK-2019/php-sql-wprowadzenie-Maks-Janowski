96f7bb21f8c8fab3dbbacfb7f8e990fae82828ab
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-10-31 14:05:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
    require_once('conn.php');
   
-    $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org');
+    $sql=('SELECT * FROM pracownicy');
     $result=$conn->query($sql);
         echo("<hr />");
         echo("<h3>Tabela Pracowników</h3>");
@@ -12,11 +12,9 @@
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
@@ -78,3 +76,4 @@ echo("<hr />");
 
 
 ?>
+
