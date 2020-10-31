53a4dd8c288a4fa48ff07e91888aec1e9fc38a6f
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-10-31 14:18:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $sql=('SELECT * from pracownicy where imie like "%a"');
             }
         echo("</table>");
 echo("<hr />");
-$sql=('SELECT * from pracownicy,organizacja where dzial=id_org  order by imie asc');
+$sql=('SELECT * from pracownicy order by imie asc');
     $result=$conn->query($sql); //mysql
         echo("<h3>Tabela Pracowników Posortowana Alfabetycznie</h3>");//nazwa nad tabelą
         echo("<li>SQL: $sql");
@@ -44,11 +44,9 @@ $sql=('SELECT * from pracownicy,organizacja where dzial=id_org  order by imie as
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>"");
                 echo("</tr>");
             }
         echo("</table>");
