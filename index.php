eb9c91e7b02ef6117c2e36bbe1c2d503b29f14db
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-10-31 14:08:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             }
         echo("</table>");
     echo("<hr />");
-    $sql=('SELECT * from pracownicy,organizacja where dzial=id_org and imie like "%a"');
+    $sql=('SELECT * from pracownicy where and imie like "%a"');
     $result=$conn->query($sql);
         echo("<h3>Tabela Kobiet</h3>");//nazwa nad tabelą
         echo("<li>SQL: $sql");
@@ -28,16 +28,14 @@
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
 echo("<hr />");
-    $sql=('SELECT * from pracownicy,organizacja where dzial=id_org  order by imie asc');
+    $sql=('SELECT * from pracownicy order by imie asc');
     $result=$conn->query($sql); //mysql
         echo("<h3>Tabela Pracowników Posortowana Alfabetycznie</h3>");//nazwa nad tabelą
         echo("<li>SQL: $sql");
@@ -46,16 +44,14 @@ echo("<hr />");
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
 echo("<hr />");
-    $sql=('SELECT * from pracownicy,organizacja where dzial=id_org  order by zarobki asc');
+    $sql=('SELECT * from pracownicy order by zarobki asc');
     $result=$conn->query($sql); //mysql
         echo("<h3>Tabela Pracowników Posortowana Zarobkami Rosnąco</h3>");//nazwa nad tabelą
         echo("<table border=1>");
@@ -64,11 +60,9 @@ echo("<hr />");
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
