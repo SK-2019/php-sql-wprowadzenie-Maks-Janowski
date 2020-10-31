cf4063c9ec3bc3566c0d133a047f3d39ff63010d
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-10-31 14:15:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             }
         echo("</table>");
     echo("<hr />");
-$sql=('SELECT * from pracownicy,organizacja where dzial=id_org and imie like "%a"');
+$sql=('SELECT * from pracownicy where imie like "%a"');
     $result=$conn->query($sql);
         echo("<h3>Tabela Kobiet</h3>");//nazwa nad tabelą
         echo("<li>SQL: $sql");
@@ -28,11 +28,9 @@ $sql=('SELECT * from pracownicy,organizacja where dzial=id_org and imie like "%a
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
