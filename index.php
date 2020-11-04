18c00d348bd2d547526bf86f67af4f9eb6a24747
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-04 14:29:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 <h1>Maksymilian Janowski 2Ti</h1>
-<a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do githuba mojego </a>
+<a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do mojego githuba </a>
 <?php
    require_once('conn.php');
 
