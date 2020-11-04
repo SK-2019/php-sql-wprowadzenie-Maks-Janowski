4a93e49867a79f044408027fa3d431db0c8cfc77
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-04 15:00:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -100,42 +100,7 @@ $sql=('SELECT * from pracownicy,organizacja where dzial=id_org  order by imie as
             }
         echo("</table>");
 echo("<hr />");
-    $sql=('SELECT dzial, max(zarobki) AS Maksimum FROM pracownicy GROUP BY dzial');
-    $result=$conn->query($sql); //mysql
-        echo("<h3>Max zarobkow</h3>");//nazwa nad tabelą
-        echo("<li>$sql");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-echo("<hr />");
-    $sql=('SELECT Dzial, sum(zarobki) AS Całość, avg(zarobki) AS Średnia FROM pracownicy GROUP BY dzial');
-    $result=$conn->query($sql); //mysql
-        echo("<h3>Suma zarobkow</h3>");//nazwa nad tabelą
-        echo("<li>$sql");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-echo("<hr />");
+
 
    
    
