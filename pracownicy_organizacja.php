e58f5f612b87f205daa05b4340a2e7ce41174ce0
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-06 16:24:47 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -110,11 +110,12 @@
         echo("<table border=1>");
         echo("<th>id</th>");
         echo("<th>imie</th>");
+        echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
         echo("<th>nazwa_dzial</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
+                echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
