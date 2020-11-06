e0fd6fca4f9a8382cb3403f96415b57c361955d5
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-06 12:58:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do mojego githuba </a>
    <a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
    <a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
+   <a class="nav_link" href="pracownicy_organizacja.php"><b>Funkcje Agregujace<b></a>
+   <a class="nav_link" href="/">Strona Glowna</a>
 <?php
    require_once('conn.php');
 
