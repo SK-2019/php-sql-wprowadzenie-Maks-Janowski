a61a29fd7b820c4a29b099ad364f7ca0330a8e38
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-06 12:53:56 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -62,6 +62,24 @@
         echo("</table>");
    
    
+            $result = $conn->query('SELECT imie, dzial, zarobki, data_urodzenia, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org and zarobki<30 and (dzial=1 or dzial=2)');
+                echo("<hr />");
+                echo("<h3>Tabela pracowników z zarobkiami mniejszymi niz 30</h3>");
+                echo("<table>");
+                echo("<th>imie</th>");
+                echo("<th>dzial</th>");
+                echo("<th>zarobki</th>");
+                echo("<th>data_urodzenia</th>");
+                
+                    while($row=$result->fetch_assoc()){ 
+                        echo("<tr>");
+                            echo("<td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>"); 
+        
+                        echo("</tr>");
+                    }
+                       echo("</table>");
+   
+   
 ?>
     </body>
 </html>
