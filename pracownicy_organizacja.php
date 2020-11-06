715dac32182ed62581b48b7440b903af9961265a
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-06 13:49:33 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -104,24 +104,24 @@
 
             
             
-                                  $sql=('select id_pracownicy, imie, nazwa_dzial, zarobki from pracownicy, organizacja where dzial=id_org order by imie desc');
-    $result=$conn->query($sql);
-        echo("<hr />");
-        echo("<h3>Tabela Pracowników</h3>");
-        echo("<li>$sql");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
+//                                   $sql=('select id_pracownicy, imie, nazwa_dzial, zarobki from pracownicy, organizacja where dzial=id_org order by imie desc');
+//     $result=$conn->query($sql);
+//         echo("<hr />");
+//         echo("<h3>Tabela Pracowników</h3>");
+//         echo("<li>$sql");
+//         echo("<table border=1>");
+//         echo("<th>id</th>");
+//         echo("<th>imie</th>");
+//         echo("<th>dzial</th>");
+//         echo("<th>zarobki</th>");
+//         echo("<th>nazwa_dzial</th>");
+//         echo("<th>data_urodzenia</th>");
+//             while($row=$result->fetch_assoc()){
+//                 echo("<tr>");
+//                 echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+//                 echo("</tr>");
+//             }
+//         echo("</table>");
       
       
       
