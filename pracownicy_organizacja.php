0ce00465798386250c5f71a1c111ddf8bc5f7ce3
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-06 13:56:15 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -99,33 +99,7 @@
         echo("</table>");
         echo("<hr />");
 
-            
-         echo("<h3>Sortowanie</h3>")
-
-            
-            
-                    $sql=('SELECT * FROM pracownicy,organizacja WHERE dzial=id_org order by imie desc');
-    $result=$conn->query($sql);
-        echo("<hr />");
-        echo("<h3>Tabela Pracowników</h3>");
-        echo("<li>$sql");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-      
-      
-      
-      
+     
       
 ?>
 </body>
