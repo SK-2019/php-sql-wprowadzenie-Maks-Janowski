156b633841e30b02f0214ccfb09bb26261429be9
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-06 16:49:19 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -107,7 +107,7 @@
       $sql=('select * from pracownicy,organizacja where dzial=id_org order by imie desc');
     $result=$conn->query($sql);
         echo("<hr />");
-        echo("<h3>Tabela pracownikow posortowanych wg imienia</h3>");
+        echo("<h3>Tabela pracownikow posortowanych malejaco wg imienia</h3>");
         echo("<li>$sql");
         echo("<table border=1>");
         echo("<th>id</th>");
@@ -122,7 +122,28 @@
                 echo("</tr>");
             }
         echo("</table>");
+      
+      
+      
+            $sql=('select * from pracownicy,organizacja where dzial=id_org and dzial=3 order by imie asc');
+    $result=$conn->query($sql);
         echo("<hr />");
+        echo("<h3>Tabela pracownikow dzialu 3 posortowanych rosnaco wg imienia</h3>");
+        echo("<li>$sql");
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa_dzial</th>");
+        echo("<th>data_urodzenia</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+
       
       
       
