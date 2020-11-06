c96620e07fb05bddcf41fc192acdeeaf78b2c4a5
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-06 12:54:58 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -69,11 +69,10 @@
                 echo("<th>imie</th>");
                 echo("<th>dzial</th>");
                 echo("<th>zarobki</th>");
-                echo("<th>data_urodzenia</th>");
                 
                     while($row=$result->fetch_assoc()){ 
                         echo("<tr>");
-                            echo("<td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>"); 
+                            echo("<td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
         
                         echo("</tr>");
                     }
