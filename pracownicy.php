a7af868e3b39ff7641c8109b8db2f9882fdfba71
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-10 13:04:31 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -9,7 +9,8 @@
    <a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
    <a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
    <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
-   <a class="nav_link" href="/">Strona Glowna</a>
+   <a class="nav_link" href="/">Strona Glowna</a>  
+   <a class="nav_link" href="data.php"><b>Data<b></a>
 <?php
     require_once('conn.php');;
     $result=$conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org');//mysql
