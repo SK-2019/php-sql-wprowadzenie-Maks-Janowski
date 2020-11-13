c48afc10dd110ca2cdd9aee03799480f99a330e8
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-13 09:09:48 +0100

    Update data.php

--- data.php
+++ data.php
@@ -12,23 +12,28 @@
    <a class="nav_link" href="/">Strona Glowna</a>  
    <a class="nav_link" href="data.php"><b>Data<b></a>
 <?php
-    require_once('conn.php');
-    $result=$conn->query('select id_pracownicy, imie, nazwa_dzial, zarobki, year(curdate())-year(data_urodzenia) as wiek from pracownicy, organizacja where id_org=dzial", "Pracownicy + wiek:');
-        echo("<hr />");
-        echo("<h3>Tabela pracowników</h3>");
-        echo("<table border=1>");
-            echo("<th>id</th>");
-	    echo("<th>Imie</th>");
-	    echo("<th>Dział</th>");
-	    echo("<th>Zarobki</th>");
-	    echo("<th>Wiek</th>");
-                while($row=$result->fetch_assoc()){
-                    echo("<tr>");
-                        echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['wiek']."</td>");
-                    echo("</tr>");
-            }
-        echo("</table>");
-   
+   require_once('conn.php');
+                        echo("<hr>");
+                        echo("<h3>Wiek poszczegolnych pracownikow (w latach)</h3>");
+            $sql=('SELECT * ,YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy,organizacja where dzial=id_org');
+                    $result=$conn->query($sql);
+                        echo("<table border=1>");
+                        echo("<li>SQL: $sql");
+                        echo("<th>id</th>");
+                        echo("<th>imie</th>");
+                        echo("<th>dzial</th>");
+                        echo("<th>zarobki</th>");
+                        echo("<th>nazwa działu</th>");
+                        echo("<th>wiek</th>");
+                        echo("<th>data_urodzenia</th>");
+                            while($row=$result->fetch_assoc()){
+                                echo("<tr>");
+                                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['wiek']."</td><td>".$row['data_urodzenia']."</td>");
+                                echo("</tr>");
+                            }
+                        echo("</table>");
+                        echo("<hr />");
+
  
    
 ?>
