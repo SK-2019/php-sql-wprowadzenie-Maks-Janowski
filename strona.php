eb2be65b7b5270011c1dca83623a014621f671f5
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-18 14:42:48 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -6,4 +6,17 @@ echo($_POST["imie"]);
 echo($_POST["Nazwisko"]);
 echo($_POST["Klasa"]);
 
+  require_once('conn.php');
+  $sql = "INSERT INTO pracownicy (imie, nazwisko)
+  VALUES ('John', 'Doe')";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+
+
+
 ?>
