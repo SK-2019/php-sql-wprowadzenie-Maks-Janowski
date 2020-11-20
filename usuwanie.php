e2aafc3eb271a56d43b8540927cf876e77d3f860
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-20 13:18:17 +0100

    Add files via upload

---
+++ usuwanie.php
@@ -0,0 +1,9 @@
+<?php
+require_once("conn.php");
+$sql =  "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."'";
+echo($sql);
+mysqli_query($conn,$sql);
+mysqli_close($conn);
+header("location:https://mj-app-app.herokuapp.com/insert.php");
+
+?>
