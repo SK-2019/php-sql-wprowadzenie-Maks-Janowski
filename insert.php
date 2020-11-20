d729e9eb5548494250ef576cd5dc79d69cdf58b0
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-20 13:41:50 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,36 +1,48 @@
-     <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>JS Bin</title>
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
- 
-  <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do mojego githuba </a>
-  <a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
-  <a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
-  <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
-  <a class="nav_link" href="/">Strona Glowna</a>  
-  <a class="nav_link" href="data.php"><b>Data<b></a>
-  <a class="nav_link" href="formularz.html"><b>Formularz<b></a>
-       
-     
-     <form action="strona.php" method="POST">
-     <input type="text" name="imie" placeholder="imie"></br>
-     <input type="text" name="dzial" placeholder="dzial"></br>
-     <input type="text" name="zarobki" placeholder="zarobki"></br>
-     <input type="text" name="data_urodzenia" placeholder="data_urodzenia"></br>
-     <input type="submit" value="wyślij do strona.php">
-     </form>
-	  
- 
-    
-	  
+<h1>Maksymilian Janowski 2Ti</h1>
+   
+   <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do mojego githuba </a>
+   <a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
+   <a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
+   <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
+   <a class="nav_link" href="/">Strona Glowna</a>  
+   <a class="nav_link" href="data.php"><b>Data<b></a>
+   <a class="nav_link" href="insert.php"><b>Formularz<b></a>
 
-       </body>
-</html>
-	  
 
+
+
+
+
+<?php
+
+echo("Witam na strona.php");
+
+echo($_POST["imie"]);
+echo($_POST["dzial"]);
+echo($_POST["zarobki"]);
+echo($_POST["data_urodzenia"]);
+
+  require_once('conn.php');
+	$sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_urodzenia']."')";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+
+
+
+?>
+	   
+</body>
+   </html>
 	  
 	  
