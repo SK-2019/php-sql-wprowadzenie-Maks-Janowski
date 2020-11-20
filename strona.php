660148653c3b93818c8df1de4baa24a6b458651c
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-20 08:12:15 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -6,7 +6,7 @@ echo($_POST["id"]);
 echo($_POST["imie"]);
 
   require_once('conn.php');
-$sql = "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (null,john,2,[70,1985-04-15)";
+$sql = "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (null,john,2,70,1985-04-15)";
   
   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
