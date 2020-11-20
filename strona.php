5f52889393edcf032978ba7303d8ad3a4ece3713
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-20 08:26:32 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -8,7 +8,7 @@ echo($_POST["zarobki"]);
 echo($_POST["data_urodzenia"]);
 
   require_once('conn.php');
-$sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_urodzenia']."')";
+	$sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_urodzenia']."')";
   
   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
