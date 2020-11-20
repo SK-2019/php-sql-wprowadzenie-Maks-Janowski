523c555c70503246a1a28ffc5cc31c4b824c9a80
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-20 13:30:59 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -4,14 +4,17 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
 <h1>Maksymilian Janowski 2Ti</h1>
-<a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do mojego githuba </a>
+   
+   <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do mojego githuba </a>
    <a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
    <a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
    <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
    <a class="nav_link" href="/">Strona Glowna</a>  
    <a class="nav_link" href="data.php"><b>Data<b></a>
+   <a class="nav_link" href="insert.php"><b>Formularz<b></a>
+      
+      
    <?php
 
    
