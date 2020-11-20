58a22c6549566ff4d7d6fc19bcab83138a7dcc06
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-20 13:41:15 +0100

    Add files via upload

---
+++ danedobazy.php
@@ -0,0 +1,34 @@
+       <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+ 
+  <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do mojego githuba </a>
+  <a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
+  <a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
+  <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
+  <a class="nav_link" href="/">Strona Glowna</a>  
+  <a class="nav_link" href="data.php"><b>Data<b></a>
+  <a class="nav_link" href="formularz.html"><b>Formularz<b></a>
+  
+  
+  
+      <h3>Dodaj pracownika do tabelki:</h3>
+  <form action="insert.php" method="POST">
+   imie: <input type="text" name="imie"></br>
+   dzial: <input type="text" name="dzial"></br>
+    zarobki: <input type="text" name="zarobki"></br>
+    data_urodzenia: <input type="text" name="data_urodzenia"></br>
+    adres: <input type="text" name="adres"></br>
+    <input type="submit" value="wyślij do insert.php">
+  </form>
+
+
+
+       </body>
+</html>
+	  
