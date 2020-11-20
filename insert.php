3d99b6e7a20ba7a77fca023a169ba4908c7e05ef
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-20 13:17:47 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -30,3 +30,30 @@
 
        </body>
 </html>
+	  
+
+	  
+	  
+ <?php
+require_once("conn.php");
+$result=$conn->query("Select * from pracownicy");
+echo("<table border=1>");
+    echo("<th>Id</th>");
+    echo("<th>Imie</th>");
+    echo("<th>Dzial</th>");
+    echo("<th>Zarobki</th>");
+    echo("<th>Data_urodzenia</th>");
+    echo("<th class=usun>Usun</th>");
+        while($row=$result->fetch_assoc()){
+            echo("<tr>");
+            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>");
+            echo("<td><form action=usuwanie.php method=POST>");
+            echo("<input type='hidden' name='id' value=".$row['id_pracownicy'].">");
+            echo("<input type=submit value=X>");
+            echo("</form></td>");
+            echo("</tr>");}
+echo("</table>");
+?>
+	  
+	  
+	  
