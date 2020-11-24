f0f0a20d3a0b0b53c46f76bec4b6e269b453ce93
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-24 13:06:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
    <a class="nav_link" href="/">Strona Glowna</a>  
    <a class="nav_link" href="data.php"><b>Data<b></a>
    <a class="nav_link" href="danedobazy.php"><b>Danedobazy<b></a>
-         <a class="nav_link" href="insert.php"><b>Formularz<b></a>
+         <a class="nav_link" href="function.php"><b>Funkcja<b></a>
         
         
 <?php
