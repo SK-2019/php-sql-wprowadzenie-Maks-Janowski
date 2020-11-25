93d5ae1b6e59749b155ab439f8cd9fe336c1c84a
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-25 14:36:08 +0100

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Maksymilian Janowski 2Ti</h1>
+   
+   <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do mojego githuba </a>
+   <a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
+   <a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
+   <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
+   <a class="nav_link" href="/">Strona Glowna</a>  
+   <a class="nav_link" href="data.php"><b>Data<b></a>
+   <a class="nav_link" href="danedobazy.php"><b>Danedobazy<b></a>
+         <a class="nav_link" href="function.php"><b>Funkcja<b></a>
+
+
+<?php
+   require_once('conn.php');
+
+   $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org');
+   $result=$conn->query($sql);
+       echo("<hr />");
+       echo("<h3>Tabela Pracowników</h3>");
+       echo("<li>$sql");
+       echo("<table border=1>");
+       echo("<th>id</th>");
+       echo("<th>imie</th>");
+       echo("<th>dzial</th>");
+       echo("<th>zarobki</th>");
+       echo("<th>nazwa_dzial</th>");
+       echo("<th>data_urodzenia</th>");
+           while($row=$result->fetch_assoc()){
+               echo("<tr>");
+               echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+               echo("</tr>");
+           }
+       echo("</table>");
+        
+
+
+
+?>
+
+
+
+</body>
+   </html>
