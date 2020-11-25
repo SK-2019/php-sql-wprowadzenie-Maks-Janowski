363a1e571e332b3fdb8b92f5c81a319f8fbea19a
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-25 14:43:52 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -19,7 +19,7 @@
 <?php
    require_once('conn.php');
 
-   $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org');
+   $sql=('SELECT * FROM pracownicy');
    $result=$conn->query($sql);
        echo("<hr />");
        echo("<h3>Tabela Pracowników</h3>");
@@ -29,11 +29,9 @@
        echo("<th>imie</th>");
        echo("<th>dzial</th>");
        echo("<th>zarobki</th>");
-       echo("<th>nazwa_dzial</th>");
-       echo("<th>data_urodzenia</th>");
            while($row=$result->fetch_assoc()){
                echo("<tr>");
-               echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+               echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
                echo("</tr>");
            }
        echo("</table>");
