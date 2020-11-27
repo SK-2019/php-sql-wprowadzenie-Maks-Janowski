126b8114fbcaeec24218581ba108acdcb66b1e32
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-11-27 08:32:41 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -18,18 +18,18 @@
 
 <?php
 
-  echo ("<h1>id: ".$_POST['id']."</h1>");
-require_once("connect.php");
+//   echo ("<h1>id: ".$_POST['id']."</h1>");
+// require_once("connect.php");
 
- $sql = "DELETE FROM pracownicy WHERE id_pracownicy='".$_POST['id']."'";
+//  $sql = "DELETE FROM pracownicy WHERE id_pracownicy='".$_POST['id']."'";
 
-if ($conn->query($sql) === TRUE) {
-        echo("<h1 class='precord'> Pracownik zostal usuniety </h1>");
-      } else {
-        echo("<h1 class='precord'>'Error: ' . $sql . '<br>' . $conn->error</h1>");
-      }
+// if ($conn->query($sql) === TRUE) {
+//         echo("<h1 class='precord'> Pracownik zostal usuniety </h1>");
+//       } else {
+//         echo("<h1 class='precord'>'Error: ' . $sql . '<br>' . $conn->error</h1>");
+//       }
 
-?>
+// ?>
 
 
 
