5dbdbb2e2e6d5cc22de8ce0566549a079413e86d
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-12-04 08:27:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
    <a class="nav_link" href="data.php"><b>Data<b></a>
    <a class="nav_link" href="danedobazy.php"><b>Danedobazy<b></a>
    <a class="nav_link" href="function.php"><b>Funkcja<b></a>
-            
+   <a class="nav_link" href="ksiazki.php"><b>Biblioteka<b></a> 
         
         
 <?php
