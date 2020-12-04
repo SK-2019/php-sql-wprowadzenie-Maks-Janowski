14f74fc3969e4590b256f0c8c2e1fca710925767
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-12-04 09:23:52 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -23,7 +23,7 @@
 
 require_once('conn.php');
 
-/*$sql=('SELECT * FROM biblTytul');
+$sql=('SELECT * FROM biblTytul');
 $result=$conn->query($sql);
     echo("<hr />");
     echo("<h3>Biblioteka Tytul</h3>");
@@ -33,11 +33,11 @@ $result=$conn->query($sql);
         while($row=$result->fetch_assoc()){
            
             /*echo("<option value=".$row['id'].">".$row['tytul']."</option>");*/
-  /*        echo("<option value=2>tytul jakis</option>");
+           echo("<option value=2>tytul jakis</option>");
         }
     echo("</select>");
 echo("<hr />");
-*/
+
       
  $sql=('SELECT * FROM biblAutor');
 $result=$conn->query($sql);
@@ -56,21 +56,21 @@ echo("<hr />");
       
       
       
-      $sql=('SELECT * FROM biblAutor_biblTytul, biblAutor, biblTytul WHERE biblAutor_id=biblAutor.id and biblTytul_id=biblTytul.id');
-$result=$conn->query($sql);
-    echo("<h3>Biblioteka Autor i Tytul</h3>");
-    echo("<li>$sql");
-    echo("<table border=1>");
-    echo("<th>id</th>");
-    echo("<th>biblAutor_id</th>");
-    echo("<th>biblTytul_id</th>");
-        while($row=$result->fetch_assoc()){
-            echo("<tr>");
-            echo("<td>".$row['id']."</td><td>".$row['biblAutor_id']."</td><td>".$row['biblTytul_id']."</td>");
-            echo("</tr>");
-        }
-    echo("</table>");
-echo("<hr />");
+//      $sql=('SELECT * FROM biblAutor_biblTytul, biblAutor, biblTytul WHERE biblAutor_id=biblAutor.id and biblTytul_id=biblTytul.id');
+// $result=$conn->query($sql);
+//     echo("<h3>Biblioteka Autor i Tytul</h3>");
+//     echo("<li>$sql");
+//     echo("<table border=1>");
+//     echo("<th>id</th>");
+//     echo("<th>biblAutor_id</th>");
+//     echo("<th>biblTytul_id</th>");
+//         while($row=$result->fetch_assoc()){
+//             echo("<tr>");
+//             echo("<td>".$row['id']."</td><td>".$row['biblAutor_id']."</td><td>".$row['biblTytul_id']."</td>");
+//             echo("</tr>");
+//         }
+//     echo("</table>");
+// echo("<hr />");
 
 
 
