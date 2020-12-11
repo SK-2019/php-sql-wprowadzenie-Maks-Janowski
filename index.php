ed5cec7e17788531d8f2540643a8c798ff2f0c21
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-12-11 09:20:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,10 +25,32 @@
        </ul>   
      </div>
       <div class="item colorGreen"> 
-     </div>
+        <?php
+   require_once('conn.php');
+
+    $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org');
+    $result=$conn->query($sql);
+        echo("<hr />");
+        echo("<h3>Tabela Pracowników</h3>");
+        echo("<li>$sql");
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa_dzial</th>");
+        echo("<th>data_urodzenia</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+     ?>
+       </div>
  
-       
- <?php
+      
+    <?php
    require_once('conn.php');
 
     $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org');
@@ -73,7 +95,7 @@ $sql=('SELECT * from pracownicy,organizacja where dzial=id_org  order by imie as
         echo("<h3>Tabela Pracowników Posortowana Alfabetycznie</h3>");//nazwa nad tabelą
         echo("<li>$sql");
         echo("<table border=1>");
-        echo("<th>id</th>");
+        echo("<th>id</th>");  
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
