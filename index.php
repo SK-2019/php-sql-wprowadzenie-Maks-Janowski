e745602f69be86dd1852a8bf310dad31ea9919d4
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-12-11 08:53:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -32,15 +32,15 @@
    <body>
 <h1>Maksymilian Janowski 2Ti</h1>
    
-   //<a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do mojego githuba </a>
-   //<a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
-   //<a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
-   //<a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
-   //<a class="nav_link" href="/">Strona Glowna</a>  
-   //<a class="nav_link" href="data.php"><b>Data<b></a>
-   //<a class="nav_link" href="danedobazy.php"><b>Danedobazy<b></a>
+   /*<a href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski"> link do mojego githuba </a>
+   <a class="nav_link" href="pracownicy.php"><b>Pracownicy<b></a>
+   <a class="nav_link" href="funkcjeagregujace.php"><b>Funkcje Agregujace<b></a>
+   <a class="nav_link" href="pracownicy_organizacja.php"><b>Pracownicy Organizacja<b></a>
+   <a class="nav_link" href="/">Strona Glowna</a>  
+   <a class="nav_link" href="data.php"><b>Data<b></a>
+   <a class="nav_link" href="danedobazy.php"><b>Danedobazy<b></a>
    <a class="nav_link" href="function.php"><b>Funkcja<b></a>
-   <a class="nav_link" href="ksiazki.php"><b>Biblioteka<b></a> 
+   <a class="nav_link" href="ksiazki.php"><b>Biblioteka<b></a> */
         
         
 <?php
