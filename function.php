884a00249803e73b1ee3cfe23e0ad415cfaa5e1b
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2020-12-16 14:30:33 +0100

    Update function.php

--- function.php
+++ function.php
@@ -1,3 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+ <body>
+    <div class="container">
+      <div class="item colorRed">
+        <h1> Maksymilian Janowski 2Ti </h1>
+
+        <h1 class="tltle"></h1>
+      </div>
+      <div class="item colorBlue">
+       <h1> Menu Boczne: </h1>
+       <ul>
+        <li class="item_link"><a class="link" href="https://github.com/SK-2019/php-sql-wprowadzenie-Maks-Janowski">Github</a></li>
+        <li class="item_link"><a class="link" href="/">Strona Glowna</a></li>
+        <li class="item_link"><a class="link" href="pracownicy.php">Pracownicy</a></li>
+        <li class="item_link"><a class="link" href="pracownicy_organizacja.php">Pracownicy Organizacja</a></li>        
+        <li class="item_link"><a class="link" href="funkcjeagregujace.php">Funkcje Agregujace</a></li>
+        <li class="item_link"><a class="link" href="data.php">Data</a></li>
+        <li class="item_link"><a class="link" href="danedobazy.php">Dane Do Bazy</a></li>
+        <li class="item_link"><a class="link" href="function.php">Funkcja</a></li>
+        <li class="item_link"><a class="link" href="ksiazki.php">Biblioteka</a></li>
+       </ul>   
+     </div>
+      <div class="item colorGreen"> 
+       <h1> Tresc Strony </h1>
+
 <?php
 
 echo("123");
@@ -18,3 +47,6 @@ jakasfunkcja();
 
 
 ?>
+        </div>
+          </body>
+        </html>
