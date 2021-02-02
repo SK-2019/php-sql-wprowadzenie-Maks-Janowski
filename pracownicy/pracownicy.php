e24359e5b3da77eb402d6d826676d4f01d11068f
Maks-Janowski <73532495+Maks-Janowski@users.noreply.github.com>
2021-02-02 13:47:23 +0100

    Update pracownicy.php

--- pracownicy/pracownicy.php
+++ pracownicy/pracownicy.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title> Maksymilian Janowski 2Ti gr2 nr11 </title>
 <link rel="stylesheet" href="../style.css">
 <link rel="icon" type="image/ico" sizes="16x16" href="../assets/favicon.ico">
  </head>
  <body>
     <div class="container">
       <div class="item colorRed">
-
+      <h1> Maksymilian Janowski 2Ti gr2 nr11 </h1>
+       
         <h1 class="tltle"></h1>
       </div>
       <div class="item colorBlue">
