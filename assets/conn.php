002044858f5cd960514c28155e058f354d57265e
Maks Janowski <janowski.maksymilian2004@gmail.com>
2021-02-23 13:39:32 +0100

    Update conn.php

--- assets/conn.php
+++ assets/conn.php
@@ -1,7 +1,7 @@
   
 <?php
 
-$conn=new mysqli($sqlservername, $sqlusername, $sqlpassword, $sqldbname);
+$conn=new mysqli($_SERVER['sqlservername'], $_SERVER['sqlusername'], $_SERVER['sqlpassword'], $_SERVER['sqldbname']);
         if($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
